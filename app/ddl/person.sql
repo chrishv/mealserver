0f1fbd82827164b8a3eab211d1c950f33015211a
unknown <MulhernB@JHC8084.jhcllp.local>
2015-06-10 09:04:56 +0100

    Changed constraint naming convention on person table

--- app/ddl/person.sql
+++ app/ddl/person.sql
@@ -6,7 +6,7 @@ CREATE TABLE person
      person_name   CHAR(100) NOT NULL,
      email_address CHAR(254) NOT NULL,
      
-     CONSTRAINT mealserver_0001 CHECK(person_id > 0),
-     CONSTRAINT mealserver_0002 CHECK(person_name <> ''),
-     CONSTRAINT mealserver_0003 CHECK(email_address <> '')
+     CONSTRAINT person_0001 CHECK(person_id > 0),
+     CONSTRAINT person_0002 CHECK(person_name <> ''),
+     CONSTRAINT person_0003 CHECK(email_address <> '')
   ); 
