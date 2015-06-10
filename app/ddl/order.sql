ee5375e4d2e06dccd6f18a7f8f460c832893a86a
priceaj <ajp@aaronjp.co.uk>
2015-06-10 09:48:52 +0100

    Add foreign Key Constraints

--- app/ddl/order.sql
+++ app/ddl/order.sql
@@ -9,4 +9,17 @@ CREATE TABLE order
    order_date   DATE
    
    CONSTRAINT order_0001  CHECK(order_type <> '')
+   
+   --Foreign Key Constraints   
+   CONSTRAINT order_FK_person_0001 
+     FOREIGN KEY (person_id)       
+     REFERENCES person(person_id)  
+     ON DELETE RESTRICT                 
+     ON UPDATE RESTRICT
+     
+   CONSTRAINT order_FK_vendor_0001 
+     FOREIGN KEY (vendor_id)       
+     REFERENCES vendor(vendor_id)  
+     ON DELETE RESTRICT                 
+     ON UPDATE RESTRICT
 ); 
