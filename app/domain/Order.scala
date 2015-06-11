34b6c4e936b4db780e773adce38c7d0d7ae74d0a
unknown <MulhernB@JHC8084.jhcllp.local>
2015-06-11 08:41:04 +0100

    Update ERD to reflect Nick's changes to person table. Add the notes into the Order class.

--- app/domain/Order.scala
+++ app/domain/Order.scala
@@ -1,4 +1,9 @@
 package uk.co.benmulhern.mealserver.domain
 
-case class Order(val id: Int, val type: String, val person_id: Int, val vendor_id: Int, val date: Date)
+case class Order(val id: Int, 
+	             val type: String, 
+	             val person_id: Int, 
+	             val vendor_id: Int, 
+	             val date: Date,
+	             val note: Option[String])
 
