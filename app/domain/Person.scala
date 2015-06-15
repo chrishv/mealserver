6306546423f2d1482abfe8541865ae55c825fad2
matthewrodgerson <matthew.rodgerson@jhc.co.uk>
2015-06-15 21:36:31 +0100

    Refactor Vendor 'PlaceOrder' into trait 'TakesOrder'

--- app/domain/Person.scala
+++ app/domain/Person.scala
@@ -1,4 +1,5 @@
 package uk.co.benmulhern.mealserver.domain
 
-case class Person(val id: Int, val forename: String, val surname: String, val email: String)
+case class Person(val id: Int, val forename: String, 
+                  val surname: String, val email: String, val usualItem:Sandwich)
 
--- app/domain/Vendor.scala
+++ app/domain/Vendor.scala
@@ -11,17 +11,11 @@ abstract class Vendor {
 }
 
 
-object Tuckers extends Vendor {
+object Tuckers extends Vendor with TakesOrder{
   
   val sausage = new Sandwich("Sausage","white","sausage",1.23)
   val bacon = new Sandwich("Bacon","white","bacon",1.45)
   val menu=List[Sandwich](sausage,bacon)
-  
-  def orderSandwiches (order:List[(Person,Sandwich)],totalSoFar:Double):Double = {
-    if (order.isEmpty) totalSoFar 
-    else if (!placeOrder(order.head._2)) throw new Error(order.head._2.sandwichName + " not on menu!")
-    else orderSandwiches(order.tail,totalSoFar+order.head._2.price)
-  }
   
   def placeOrder(sandwich:Sandwich):Boolean = {
     menu.contains(sandwich)
