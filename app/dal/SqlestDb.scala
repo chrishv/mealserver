705775b84162a4423b862db0d39f2137461714ce
matthewrodgerson <matthew.rodgerson@jhc.co.uk>
2015-07-03 12:50:59 +0100

    Vendor addition as modal window
    Try again

--- app/dal/SqlestDb.scala
+++ app/dal/SqlestDb.scala
@@ -3,15 +3,25 @@ package dal
 import sqlest._
 import play.api.db._
 import play.api.Play.current
+import com.ibm.as400.access.AS400JDBCConnectionPoolDataSource
 
 trait SqlestDb {
 
-  val dataSource = DB.getDataSource()
-
-  val statementBuilder = sqlest.sql.H2StatementBuilder
-
-  implicit val database = Database.withDataSource(dataSource, statementBuilder)
-    
+  //val dataSource = DB.getDataSource()
+    val dataSource = {
+    val dataSource = new AS400JDBCConnectionPoolDataSource("tracey.servers.jhc.co.uk")
+    dataSource.setUser("RTUSER")
+    dataSource.setPassword("RTUSER")
+    dataSource.setLibraries("W.MJR")
+    dataSource.setNaming("system")
+    dataSource.setPrompt(false)
+    dataSource
+  }
+  //val statementBuilder = sqlest.sql.H2StatementBuilder
+    val statementBuilder = sqlest.sql.DB2StatementBuilder
+  
+  //  implicit val database = Database.withDataSource(dataSource, statementBuilder)
+    implicit val database = Database.withDataSource(dataSource, statementBuilder)  
   // Try to create the new DB on the first go
   try {
     CreateNewDb.generateNewDb()
